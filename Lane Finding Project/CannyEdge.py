bfe2d8bbe2a8abbce7c6b173f29715d23ec5c70e
Bruce Chan <jianle001@e.ntu.edu.sg>
2019-07-04 21:32:03 +0800

    Project 1 - UdacitySolution

---
+++ Lane Finding Project/CannyEdge.py
@@ -0,0 +1,24 @@
+import cv2
+
+
+# Load the image
+img = cv2.imread('exit-ramp.jpg')
+
+# Covert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Guassian Blur, note that it always take a odd number
+kernel_size = 3
+blur_gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
+
+# CannyEdge
+low_threshold = 50
+high_threshold = 255
+edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+# Display the image
+cv2.imshow('CannyEdge', edges)
+
+# Close the windows properly
+cv2.waitKey(0)
+cv2.destroyAllWindows()
